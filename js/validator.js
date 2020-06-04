e9daf84dcc9d0faf830974cf41388f668343958e
Szymon Dąbrowski <szymonmiloszdabrowski@gmail.com>
2020-06-04 16:09:19 +0200

    multi-click button bug fixed

--- js/validator.js
+++ js/validator.js
@@ -272,7 +272,7 @@ function sprawdzWszystko(){
     var image = sprawdzImage();
     //console.log(name, number, netto, vat, kategoria, opcje, ocena, image)
     if(!(name || number || netto || vat || kategoria || opcje || ocena || image)){
-        obslugaTabeli();
+        dodajProdukt();
     }
 }
 function sprawdzIloscSztuk(){
@@ -282,6 +282,9 @@ function sprawdzIloscSztuk(){
     var factor = Math.pow(10, 0);
     for(var i=0;i<tabSztuki.length;i++){
         tabSztuki[i].value = Math.round(tabSztuki[i].value)/factor;
+        if(tabSztuki[i].value<0){
+            tabSztuki[i].value=0
+        }
         razem += tabCeny[i].innerHTML*tabSztuki[i].value;
     }
     
@@ -298,8 +301,10 @@ function sprawdzIloscSztuk(){
 //koniec walidacji **********************************************************************************
 
 $("#myTable").tablesorter({ sortList: [[0,0]] });
+$("#tableKoszyk").tablesorter({ sortList: [[0,0]] });
+var nazwaICena = [];
 
-function obslugaTabeli(){
+function dodajProdukt(){
     var name = document.getElementById("name").value;
     var istniejaceProdukty = document.getElementsByClassName("nameInTable");
     for(var i=0; i<istniejaceProdukty.length; i++){
@@ -328,133 +333,118 @@ function obslugaTabeli(){
         }
     }
 
-    var row = '<tr><td class="nameInTable">'+name+'</td><td>'+number+'</td><td>'+netto+'</td><td>'+vat+'</td><td>'+brutto+'</td><td>'+kategoria+'</td><td>'+wybraneOpcje+'</td><td>'+wybranaOcena+'</td> <td>'+image+'</td><td><button type="button" class="removeRow btn btn-danger" style="margin-bottom: 5px;" title="Usuń wiersz"><i class="icon-cancel"></i></button>'+" "+'<button type="button" class="edit btn btn-warning" style="margin-bottom: 5px;" title="Edytuj wiersz"><i class="icon-pencil"></i></button>'+" "+'<button type="button" class="koszyk btn btn-success" style="margin-bottom: 5px;" title="Dodaj do koszyka"><i class="icon-basket"></i></button></td></tr>';
+    var row = '<tr><td class="nameInTable">'+name+'</td><td>'+number+'</td><td>'+netto+'</td><td>'+vat+'</td><td>'+brutto+'</td><td>'+kategoria+'</td><td>'+wybraneOpcje+'</td><td>'+wybranaOcena+'</td> <td>'+image+'</td><td><button type="button" class="btn btn-danger" style="margin-bottom: 5px;" title="Usuń wiersz" onClick="usunProdukt()"><i class="icon-cancel"></i></button>'+" "+'<button type="button" class="btn btn-warning" style="margin-bottom: 5px;" title="Edytuj wiersz" onClick="edytujProdukt()"><i class="icon-pencil"></i></button>'+" "+'<button type="button" class="btn btn-success" style="margin-bottom: 5px;" title="Dodaj do koszyka" onClick="dodajDoKoszyka()"><i class="icon-basket"></i></button></td></tr>';
     $row = $(row),
     resort = true;
 
-    //dodaj produk
     $('#myTable')
         .find('tbody').append($row)
         .trigger('addRow', [$row, resort])
         .trigger('update');
-
-    //usun produkt
-    $('#myTable').delegate('button.removeRow', 'click',
-    function() {
-        var t = $('#myTable');
-        $(this).closest('tr').remove();
-        t.trigger('update');
-    });
-    //usun produkt
-    $('#tableKoszyk').delegate('button.removeProduct', 'click',
-    function() {
-        var t = $('#tableKoszyk');
-        $(this).closest('tr').remove();
-        localStorage.removeItem('koszyk', );
-        t.trigger('update');
-    });
-
-    //edytuj produkt
-    $('button.edit').click(function() {
-        var thisRow = $(this).closest('tr').find('td');
-        document.getElementById("name").value = thisRow[0].innerHTML;
-        document.getElementById("number").value = thisRow[1].innerHTML;
-        document.getElementById("netto").value = thisRow[2].innerHTML;
-        document.getElementById("vat").value = thisRow[3].innerHTML;
-        document.getElementById("brutto").value = thisRow[4].innerHTML;
-        document.getElementById("kategoria").value = thisRow[5].innerHTML;
-        document.getElementById("image").value = thisRow[8].innerHTML;
-
-        for(var i=0;i<document.getElementsByName("opcja").length;i++){
-            document.getElementsByName("opcja")[i].checked=false
-        }
-        if(thisRow[6].innerHTML.indexOf("1")!=-1)
-            document.getElementsByName("opcja")[0].checked=true;
-        if (thisRow[6].innerHTML.indexOf("2")!=-1)
-            document.getElementsByName("opcja")[1].checked=true;
-        if (thisRow[6].innerHTML.indexOf("3")!=-1)
-            document.getElementsByName("opcja")[2].checked=true;
-        if (thisRow[6].innerHTML.indexOf("4")!=-1)
-            document.getElementsByName("opcja")[3].checked=true;
-        if (thisRow[6].innerHTML.indexOf("5")!=-1)
-            document.getElementsByName("opcja")[4].checked=true;
-        sprawdzOpcje()
-
-        switch (thisRow[7].innerHTML){
-            case "1/5":
-                document.getElementsByName("rate")[0].checked=true;
-                break;
-            case "2/5":
-                document.getElementsByName("rate")[1].checked=true;
-                break;
-            case "3/5":
-                document.getElementsByName("rate")[2].checked=true;
-                break;
-            case "4/5":
-                document.getElementsByName("rate")[3].checked=true;
-                break;
-            case "5/5":
-                document.getElementsByName("rate")[4].checked=true;
-                break;
-        }
-        sprawdzOcene()
-        document.getElementById("buttonAdd").style.visibility = "hidden";
-        document.getElementById("buttonEdit").style.visibility = "visible";
-        $('#buttonEdit').click(function() {
-            document.getElementById("buttonAdd").style.visibility = "visible";
-            document.getElementById("buttonEdit").style.visibility = "hidden";
-            var r = document.getElementById("myTable").rows;
-            for(var i=1; i<r.length; i++){
-                var c = r[i].cells;
-                if(c[0].innerHTML===document.getElementById("name").value){
-                    r[i].remove();
-                    break;
-                }
-            }
-            $('#myTable').trigger('update');
-            sprawdzWszystko();
-        });
-    });
-
-    //dodaj do koszyka
-    $('button.koszyk').click(function() {
-        var komorka = $(this).closest('tr').find('td');
-        var produkt = [komorka[0].innerHTML, komorka[4].innerHTML]
-        for(var i=0;i<nazwaICena.length;i++){
-            if(produkt[0]===nazwaICena[i][0]){
-                alert("Ten produkt jest już w koszyku")
-                return false;
-            }
-        }
-        nazwaICena.push(produkt);
-        var zebraneDane = JSON.stringify(nazwaICena);
-        localStorage.setItem('koszyk', zebraneDane);
-
-        var koszykAktualny = localStorage.getItem('koszyk');
-        var daneDoTabelki = JSON.parse(koszykAktualny);
-
-        $("#tableKoszyk").tablesorter({ sortList: [[0,0]] });
-        var row = '<tr><td>'+daneDoTabelki[daneDoTabelki.length-1][0]+'</td><td class="cena">'+daneDoTabelki[daneDoTabelki.length-1][1]+'</td><td><input name="iloscSztuk" type="number" onChange="sprawdzIloscSztuk()" value="1"></td></tr>'
-        $row = $(row),
-        resort = true;
-        $("#tableKoszyk")
-            .find('tbody').append($row)
-            .trigger('addRow', [$row, resort])
-            .trigger('update');
-        sprawdzIloscSztuk()
-        $("#myModal").modal()
-    });
-
-    $("#kup").click(function() {
-        localStorage.clear();
-        $('#tableKoszyk').find('tbody tr').remove();
-        $('#tableKoszyk').trigger('update');
-        document.getElementById("razem").innerHTML = 0 + " zł";
-        alert("Zakup przebiegł pomyślnie!");
-    });
-    return false;
 }
-var nazwaICena = [];
+
+function usunProdukt(){
+    $(document.activeElement).closest('tr').remove();
+    $('#myTable').trigger('update');
+}
+
+function edytujProdukt(){
+    var thisRow = $(document.activeElement).closest('tr').find('td');
+    document.getElementById("name").value = thisRow[0].innerHTML;
+    document.getElementById("number").value = thisRow[1].innerHTML;
+    document.getElementById("netto").value = thisRow[2].innerHTML;
+    document.getElementById("vat").value = thisRow[3].innerHTML;
+    document.getElementById("brutto").value = thisRow[4].innerHTML;
+    document.getElementById("kategoria").value = thisRow[5].innerHTML;
+    document.getElementById("image").value = thisRow[8].innerHTML;
+
+    for(var i=0;i<document.getElementsByName("opcja").length;i++){
+        document.getElementsByName("opcja")[i].checked=false
+    }
+    if(thisRow[6].innerHTML.indexOf("1")!=-1)
+        document.getElementsByName("opcja")[0].checked=true;
+    if (thisRow[6].innerHTML.indexOf("2")!=-1)
+        document.getElementsByName("opcja")[1].checked=true;
+    if (thisRow[6].innerHTML.indexOf("3")!=-1)
+        document.getElementsByName("opcja")[2].checked=true;
+    if (thisRow[6].innerHTML.indexOf("4")!=-1)
+        document.getElementsByName("opcja")[3].checked=true;
+    if (thisRow[6].innerHTML.indexOf("5")!=-1)
+        document.getElementsByName("opcja")[4].checked=true;
+    sprawdzOpcje()
+
+    switch (thisRow[7].innerHTML){
+        case "1/5":
+            document.getElementsByName("rate")[0].checked=true;
+            break;
+        case "2/5":
+            document.getElementsByName("rate")[1].checked=true;
+            break;
+        case "3/5":
+            document.getElementsByName("rate")[2].checked=true;
+            break;
+        case "4/5":
+            document.getElementsByName("rate")[3].checked=true;
+            break;
+        case "5/5":
+            document.getElementsByName("rate")[4].checked=true;
+            break;
+    }
+    sprawdzOcene()
+    document.getElementById("buttonAdd").style.visibility = "hidden";
+    document.getElementById("buttonEdit").style.visibility = "visible";
+}
+
+function zapiszZmiany(){
+    document.getElementById("buttonAdd").style.visibility = "visible";
+    document.getElementById("buttonEdit").style.visibility = "hidden";
+    var r = document.getElementById("myTable").rows;
+    for(var i=1; i<r.length; i++){
+        var c = r[i].cells;
+        if(c[0].innerHTML===document.getElementById("name").value){
+            r[i].remove();
+            break;
+        }
+    }
+    $('#myTable').trigger('update');
+    sprawdzWszystko();
+}
+
+function dodajDoKoszyka() {
+    var komorka = $(document.activeElement).closest('tr').find('td');
+    var produkt = [komorka[0].innerHTML, komorka[4].innerHTML]
+    for(var i=0;i<nazwaICena.length;i++){
+        if(produkt[0]===nazwaICena[i][0]){
+            alert("Ten produkt jest już w koszyku")
+            return false;
+        }
+    }
+    nazwaICena.push(produkt);
+    var zebraneDane = JSON.stringify(nazwaICena);
+    localStorage.setItem('koszyk', zebraneDane);
+
+    var koszykAktualny = localStorage.getItem('koszyk');
+    var daneDoTabelki = JSON.parse(koszykAktualny);
+
+    var row = '<tr><td>'+daneDoTabelki[daneDoTabelki.length-1][0]+'</td><td class="cena">'+daneDoTabelki[daneDoTabelki.length-1][1]+'</td><td><input name="iloscSztuk" type="number" onChange="sprawdzIloscSztuk()" value="1"></td></tr>'
+    $row = $(row),
+    resort = true;
+    $("#tableKoszyk")
+        .find('tbody').append($row)
+        .trigger('addRow', [$row, resort])
+        .trigger('update');
+    sprawdzIloscSztuk()
+    $("#myModal").modal()
+}
+
+function kupProdukty(){
+    localStorage.clear();
+    $('#tableKoszyk').find('tbody tr').remove();
+    $('#tableKoszyk').trigger('update');
+    nazwaICena = [];
+    document.getElementById("razem").innerHTML = 0 + " zł";
+    alert("Zakup przebiegł pomyślnie!");
+}
 
 function funSortowanie(){
     var obj=document.getElementById("sortowanie");
